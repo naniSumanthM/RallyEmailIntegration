b4fafdc99913f90b8b3f138d6052814676bea648
maddirsh <maddirsh@mail.uc.edu>
2017-02-09 18:06:20 -0500

    featureTesting
    Feature is read only, so we need to pre make the features on Rally's
    front end and use those references.
    MakeUserStory() works.

--- Rally/Demo.cs
+++ Rally/Demo.cs
@@ -8,18 +8,9 @@ namespace Rally
         {
             RallyOperation operation = new RallyOperation(RallyConstant.UserName, RallyConstant.Password);
 
-            //operation.Sync(RallyQueryConstant.WorkspaceZScratch, RallyQueryConstant.ScrumTeamSampleProject);  
-            //operation.CreateUserStory("IterationStory",
-            //                           "We will pass in an already created iteration from Rally", 
-            //                           RallyQueryConstant.WorkspaceZScratch, 
-            //                           RallyQueryConstant.ScrumTeamSampleProject, 
-            //                           RallyQueryConstant.RallyUserJostte, 
-            //                           RallyQueryConstant.Iteration, 
-            //                           "2");
-
-
-            operation.getIterations(RallyQueryConstant.WorkspaceZScratch, RallyQueryConstant.ScrumTeamSampleProject);
-
+            operation.CreateUserStory(RallyQueryConstant.WorkspaceZScratch, RallyQueryConstant.ScrumTeamSampleProject, "ABC");
+            
+             
             Console.ReadLine();
         }
     }
