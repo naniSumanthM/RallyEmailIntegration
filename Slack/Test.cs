2cc4471800c0a9775d7edd8c92d70709a3d409b9
maddirsh <maddirsh@mail.uc.edu>
2017-03-06 15:03:01 -0500

    slack
    Slack Testing
    TODO: tbd

---
+++ Slack/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slack
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string urlWithAccessToken = "https://hooks.slack.com/services/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";
+
+            SlackClient client = new SlackClient(urlWithAccessToken);
+
+            client.PostMessage(username: "sumanth", text: "SlackBot", channel: "#general");
+
+            Console.ReadLine();
+        }
+    }
+}
