68c3bb299afa9588fe24f9161a294554a6850807
maddirsh <maddirsh@mail.uc.edu>
2017-03-08 14:34:43 -0500

    slackPilot
    Able to send messages, and determined to make a Slack notification
    system

--- Slack/Test.cs
+++ Slack/Test.cs
@@ -7,17 +7,18 @@ namespace Slack
         static void Main(string[] args)
         {
             SlackClient client = new SlackClient(SlackConstant.webhookURL);
-            client.PostMessage(username: "sumanth", text: "SlackBot", channel: "#general");
 
-
+            for (int i = 0; i < 5; i++)
+            {
+                client.PostMessage
+                   (
+                       username: "sumanth",
+                       text: "Notification Service"+i,
+                       channel: "#general"
+                   );
+            }
 
             Console.ReadLine();
         }
     }
 }
-
-
-//can iterate throgh the list of messages, create the user story, and create a slack notification with the title of the user story
-//THis might become spammy becuase users cannot see the story from slack.
-
-//Purpose: 
